9e5aebbf87ce9a91de6905bf3b38c0967a10fd60
Szabó Dániel Ernő <r3ap3rpy@gmail.com>
2026-07-23 09:19:04 +0200

    Progressed with closured

---
+++ closures/src/main.rs
@@ -0,0 +1,42 @@
+use std::mem;
+
+fn main() {
+    let outer_variable = 42;
+    let closure_annotated = | i: i32 | -> i32 { i + outer_variable };
+    let closure_not_annotated = | i | { i + outer_variable };
+
+    println!("Closure annotated: {}", closure_annotated(10));
+    println!("Closure inferrred: {}", closure_not_annotated(5));
+
+    let one = || 1;
+    println!("The one(): {}",one());
+
+    let color = String::from("green");
+
+    // print only holds an immutable reference
+    let print = || println!("color: {}",color);
+    print();
+    let _reborrow = &color;
+    let color_moved = color;
+
+    let mut count = 0;
+    let mut inc = ||  {
+        count += 1;
+        println!("Count: {}",count);
+    };
+    inc();
+    inc();
+    inc();
+
+    let movable = Box::new(3);
+    let consume = || {
+        println!("Movable: {:?}",movable);
+        drop(movable);
+    };
+
+    consume();
+
+    let haystack = vec![1,2,3];
+    let contains = move |needle| haystack.contains(needle);
+    println!("Haystack contains 2 : {}",contains(&2));
+}
