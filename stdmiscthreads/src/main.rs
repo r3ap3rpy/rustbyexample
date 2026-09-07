22b2b319370fcf32d05ed9993970a3fe34e3b41b
Szabó Dániel Ernő <r3ap3rpy@gmail.com>
2026-09-07 17:37:06 +0200

    stdmiscthreads

---
+++ stdmiscthreads/src/main.rs
@@ -0,0 +1,15 @@
+use std::thread;
+
+const NTHREADS: u32 = 8;
+
+fn main() {
+    let mut children = vec![];
+    for i in 0..NTHREADS {
+        children.push(thread::spawn(move||{
+            println!("This is thread number: {}",i);
+        }));
+    }
+    for child in children {
+        let _ = child.join();
+    }
+}
