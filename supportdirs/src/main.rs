226cf4f4f145275f29c596c86b4a7ba6330040fe
Szabó Dániel Ernő <r3ap3rpy@gmail.com>
2026-09-08 20:40:26 +0200

    supportdirs

---
+++ supportdirs/src/main.rs
@@ -0,0 +1,37 @@
+use std::env::{current_exe};
+use std::path::{PathBuf};
+use std::fs::create_dir_all;
+use std::fmt;
+
+struct Folders {
+    logs: PathBuf,
+    config: PathBuf,
+}
+
+impl fmt::Debug for Folders {
+    fn fmt(&self, f: &mut fmt::Formatter)->fmt::Result{
+        f.debug_struct("Folders").field("logs",&self.logs).field("config",&self.config).finish()
+    }
+}
+
+fn verify_app_folder_state(exe_path: &PathBuf) -> Result<Folders,Box<dyn std::error::Error>> {
+    let app_dir = exe_path.parent().expect("Cannot reach parent folder!");
+    let config_dir = app_dir.join("config");
+    let logs_dir = app_dir.join("logs");
+    create_dir_all(&config_dir)?;
+    create_dir_all(&logs_dir)?;
+    Ok(Folders { logs: logs_dir, config: config_dir })
+}
+
+
+fn main() {
+    let executable_path = current_exe().expect("Cannot access binary!");
+
+    let folders = match verify_app_folder_state(&executable_path) {
+        Ok(folders) => folders,
+        Err(e) => panic!("Folder state is incomplete: {}",e)
+    };
+    println!("Executable: {:?}",executable_path);
+    println!("The configuration folder: {:?}",folders.config);
+    println!("The logs folder: {:?}",folders.logs);
+}
