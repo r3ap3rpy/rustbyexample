a01bc8407e68627863521f6d85127f75cda17cf6
Szabó Dániel Ernő <r3ap3rpy@gmail.com>
2026-09-09 20:19:30 +0200

    removed useless stuff

--- supportdirs/src/main.rs
+++ supportdirs/src/main.rs
@@ -1,9 +1,9 @@
-use std::fs::File;
-use std::io::{self, IsTerminal, Write};
 use std::env::current_exe;
-use std::path::PathBuf;
+use std::fs::File;
 use std::fs::create_dir_all;
-use std::time::{UNIX_EPOCH,SystemTime};
+use std::io::{self, IsTerminal, Write};
+use std::path::PathBuf;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 #[derive(Debug)]
 enum LogLevels {
@@ -36,30 +36,32 @@ impl Logger {
             LogLevels::Error => "ERROR",
             LogLevels::Critical => "CRITICAL",
         };
-        let timestamp = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
+        let timestamp = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
         let message = format!("[{timestamp:^12}] :: [{level:^10}] -> {message}");
-        writeln!(self.file,"{message}")?;
+        writeln!(self.file, "{message}")?;
         if self.is_terminal {
             println!("{message}");
         }
         Ok(())
     }
     fn info(&mut self, message: &str) -> io::Result<()> {
-        self.log(LogLevels::Info,message)
+        self.log(LogLevels::Info, message)
     }
-    fn debug(&mut self,message: &str) -> io::Result<()> {
-        self.log(LogLevels::Debug,message)
+    fn debug(&mut self, message: &str) -> io::Result<()> {
+        self.log(LogLevels::Debug, message)
     }
-    fn warning(&mut self,message: &str) -> io::Result<()> {
-        self.log(LogLevels::Warning,message)
+    fn warning(&mut self, message: &str) -> io::Result<()> {
+        self.log(LogLevels::Warning, message)
     }
-    fn error(&mut self,message: &str) -> io::Result<()> {
-        self.log(LogLevels::Error,message)
+    fn error(&mut self, message: &str) -> io::Result<()> {
+        self.log(LogLevels::Error, message)
     }
-    fn critical(&mut self,message: &str) -> io::Result<()> {
-        self.log(LogLevels::Critical,message)
+    fn critical(&mut self, message: &str) -> io::Result<()> {
+        self.log(LogLevels::Critical, message)
     }
-
 }
 
 #[derive(Debug)]
@@ -68,30 +70,36 @@ struct Folders {
     config: PathBuf,
 }
 
-fn verify_app_folder_state(exe_path: &PathBuf) -> Result<Folders,Box<dyn std::error::Error>> {
+fn verify_app_folder_state(exe_path: &PathBuf) -> Result<Folders, Box<dyn std::error::Error>> {
     let app_dir = exe_path.parent().expect("Cannot reach parent folder!");
     let config_dir = app_dir.join("config");
     let logs_dir = app_dir.join("logs");
     create_dir_all(&config_dir)?;
     create_dir_all(&logs_dir)?;
-    Ok(Folders { logs: logs_dir, config: config_dir })
+    Ok(Folders {
+        logs: logs_dir,
+        config: config_dir,
+    })
 }
-
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // choose this, because if current_dir is used and the cargo run command is ran
     // the relative location is the current folder where the cargo run was issued not where the
     // binary lives
     let executable_path = current_exe().expect("Cannot access binary!");
-    let mut logfile_name = executable_path.file_name().unwrap().to_string_lossy().into_owned();
+    let mut logfile_name = executable_path
+        .file_name()
+        .unwrap()
+        .to_string_lossy()
+        .into_owned();
     logfile_name.push_str(".log");
     let folders = match verify_app_folder_state(&executable_path) {
         Ok(folders) => folders,
-        Err(e) => panic!("Folder state is incomplete: {}",e)
+        Err(e) => panic!("Folder state is incomplete: {}", e),
     };
-    println!("Executable: {:?}",executable_path);
-    println!("The configuration folder: {:?}",folders.config);
-    println!("The logs folder: {:?}",folders.logs);
+    println!("Executable: {:?}", executable_path);
+    println!("The configuration folder: {:?}", folders.config);
+    println!("The logs folder: {:?}", folders.logs);
     let mut app_logger = Logger::new(&folders.logs, &logfile_name)?;
     app_logger.info("This is an informational level message!")?;
     std::thread::sleep(std::time::Duration::from_secs(5));
@@ -99,5 +107,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     app_logger.warning("5 seconds later")?;
     app_logger.error("5 seconds later")?;
     app_logger.critical("5 seconds later")?;
-     Ok(())
+    Ok(())
 }
